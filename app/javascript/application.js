481ce2a956a04e6a1ea27cca141a51d1b6dee815
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-02-08 11:17:14 -0300

    :fire: fix: remove node modules

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+// ....
+import "bootstrap";
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+// ....
+
+import "./stylesheets/application";
+require("admin-lte");
+import "@fortawesome/fontawesome-free/js/all";
